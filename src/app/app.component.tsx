5d6a14751fba614da79a3634697a40a5dfa92ad3
Trotyl Yu <trotyl@qq.com>
2018-12-25 15:52:30 +0800

    feat: add required dependencies

---
+++ src/app/app.component.tsx
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { createElement, Renderable } from 'ng-vdom';
+
+@Component({
+  selector: 'app-root',
+  template: '',
+})
+export class AppComponent extends Renderable {
+  title = 'ng-renderable-mvc';
+
+  render() {
+    return (
+      <section className="todoapp"></section>
+    );
+  }
+}
