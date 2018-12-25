4550cf6a8199ef09d2c7e2eb5ae0aae1f5bac27d
Trotyl Yu <trotyl@qq.com>
2018-12-25 16:13:41 +0800

    feat: add static header layout

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,16 +1,58 @@
-import { Component } from '@angular/core'
+import { Component, Injector } from '@angular/core'
 import { createElement, Renderable } from 'ng-vdom'
+
+let counter = 0
 
 @Component({
   selector: 'app-root',
   template: '',
 })
 export class AppComponent extends Renderable {
-  title = 'ng-renderable-mvc'
+  newTodoTitle = ''
+
+  constructor(injector: Injector) {
+    super(injector)
+
+    this.onNewTodoChange = this.onNewTodoChange.bind(this)
+    this.onNewTodoSubmit = this.onNewTodoSubmit.bind(this)
+  }
 
   render() {
+    console.log(`rendered ${++counter} times`)
+
     return (
-      <section className="todoapp"></section>
+      <section className="todoapp">
+        <Header title={this.newTodoTitle} onChange={this.onNewTodoChange} onSubmit={this.onNewTodoSubmit} />
+      </section>
     )
+  }
+
+  onNewTodoChange(title: string) {
+    this.newTodoTitle = title
+  }
+
+  onNewTodoSubmit() {
+    this.newTodoTitle = ''
+  }
+}
+
+function Header(props: { title: string, onChange: (title: string) => void, onSubmit: () => void }) {
+  const { title, onChange, onSubmit } = props
+
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <input className="new-todo" placeholder="What needs to be done?" autofocus value={title} onInput={onInputInput} onKeyup={onInputKeyup} />
+    </header>
+  )
+
+  function onInputInput(event: KeyboardEvent) {
+    onChange((event.target as HTMLInputElement).value)
+  }
+
+  function onInputKeyup(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      onSubmit()
+    }
   }
 }
