4235e1d145bd4ab0ebcb6fb112764b4e505f7bb3
Trotyl Yu <trotyl@qq.com>
2018-12-25 16:28:03 +0800

    fear: add footer

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -3,18 +3,31 @@ import { createElement, Renderable } from 'ng-vdom'
 
 let counter = 0
 
+export interface Todo {
+  title: string
+  completed: boolean
+}
+
 @Component({
   selector: 'app-root',
   template: '',
 })
 export class AppComponent extends Renderable {
   newTodoTitle = ''
+  todos: Todo[] = []
 
   constructor(injector: Injector) {
     super(injector)
 
     this.onNewTodoChange = this.onNewTodoChange.bind(this)
     this.onNewTodoSubmit = this.onNewTodoSubmit.bind(this)
+    this.onClearCompleted = this.onClearCompleted.bind(this)
+
+    const restored = localStorage.getItem('todos')
+
+    if (restored) {
+      this.todos = JSON.parse(restored)
+    }
   }
 
   render() {
@@ -23,6 +36,7 @@ export class AppComponent extends Renderable {
     return (
       <section className="todoapp">
         <Header title={this.newTodoTitle} onChange={this.onNewTodoChange} onSubmit={this.onNewTodoSubmit} />
+        <Footer list={this.todos} onClear={this.onClearCompleted} />
       </section>
     )
   }
@@ -32,7 +46,25 @@ export class AppComponent extends Renderable {
   }
 
   onNewTodoSubmit() {
+    this.todos.push({ title: this.newTodoTitle, completed: false })
     this.newTodoTitle = ''
+
+    this.commit()
+  }
+
+  onClearCompleted() {
+    for (let i = this.todos.length - 1; i >= 0; i--) {
+      if (this.todos[i].completed) {
+        this.todos.splice(i, 1)
+      }
+    }
+
+    this.commit()
+  }
+
+  private commit() {
+    const serialized = JSON.stringify(this.todos)
+    localStorage.setItem('todos', serialized)
   }
 }
 
@@ -54,5 +86,26 @@ function Header(props: { title: string, onChange: (title: string) => void, onSub
     if (event.key === 'Enter') {
       onSubmit()
     }
+  }
+}
+
+function Footer(props: { list: Todo[], onClear: () => void }) {
+  const { list, onClear } = props
+
+  const remaining = list.filter(x => !x.completed).length
+
+  return list.length > 0 && (
+    <footer className="footer">
+      <span className="todo-count">
+        <strong>{remaining}</strong>
+        &nbsp;{remaining === 1 ? 'item' : 'items'}
+        &nbsp;left
+      </span>
+      {remaining > 0 && <button className="clear-completed" onClick={onButtonClick}>Clear completed</button>}
+    </footer>
+  )
+
+  function onButtonClick() {
+    onClear()
   }
 }
